a48f7b45ab8f0965bcd51699c94b6a0f18dc0374
Md. Kawsar Ahmed <knownaskawsar@gmail.com>
2024-12-09 17:31:07 +0600

    Update UserCard.tsx

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -8,7 +8,7 @@ export default function UserCard({type}: {type: string}) {
             <span className='text-[10px] bg-white px-2 py-1 rounded-full text-green-500'>
                 2024/25
             </span>
-        <Image src="/more.png" width={20} height={20}></Image>
+        <Image src="/more.png" alt="Student Icon" width={20} height={20}></Image>
         </div>
     <h1 className='text-2xl font-semibold my-4'>1,324</h1>
     <h2 className='capitalize text-sm font-medium text-gray-800'>{type}</h2>
